1149811b6203f927525706207c1bc1229dca46db
zhaoming <zziazm@126.com>
2016-10-11 20:54:29 +0800

    add voice image

---
+++ RecordAudio/RecordAudio/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecordAudio
+//
+//  Created by 赵铭 on 16/8/29.
+//  Copyright © 2016年 zziazm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
